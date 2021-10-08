db68cffbd071605d5ac7be3fc906e644b7c99461
corvinus-jn7nxu <bence.molics@stud.uni-corvinus.hu>
2021-10-08 15:47:28 +0200

    until Task 7

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace week04
 {
     public partial class Form1 : Form
     {
+
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
+        }
+
+        private void CreateExcel()
+        {
+
+            try
+            {
+                xlApp = new Excel.Application(); // Excel elindítása és az applikáció objektum betöltése
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value); //új munkafüzet
+
+                xlSheet = xlWB.ActiveSheet; // Új munkalap
+
+                //CreateTAble();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        
         }
     }
 }
