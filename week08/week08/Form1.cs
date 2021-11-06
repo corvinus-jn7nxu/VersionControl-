ac4649368031e20fa8335851c44f5052b842c80f
corvinus-jn7nxu <bence.molics@stud.uni-corvinus.hu>
2021-11-06 18:40:34 +0100

    Futoszalag

--- week08/week08/Form1.cs
+++ week08/week08/Form1.cs
@@ -7,14 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week08.Entities;
 
 namespace week08
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
         }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var b = Factory.CreateNew();
+            b.Left = b.Width * -1;
+            _balls.Add(b);
+            mainPanel.Controls.Add(b);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var Position = 0;
+            foreach (Ball item in _balls)
+            {
+                item.MoveBall();
+                if (Position < item.Left)
+                {
+                    Position = item.Left;
+                }
+            }
+            
+            if (Position >= 1000)
+            {
+                //Ball Saveb = new Ball();
+                var Saveb = _balls[0];
+                mainPanel.Controls.Remove(Saveb);
+                _balls.Remove(Saveb);
+                //_balls.RemoveAt(0);
+                //mainPanel.Controls.RemoveAt(0);
+
+            }
+
+        }
+
     }
 }
