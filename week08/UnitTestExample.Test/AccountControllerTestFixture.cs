9ae78d2a11694dd05863cd53d77ffe4307d89edc
corvinus-jn7nxu <bence.molics@stud.uni-corvinus.hu>
2021-11-26 18:35:15 +0100

    password test

--- week08/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ week08/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -26,6 +26,23 @@ namespace UnitTestExample.Test
 
             //assert
             Assert.AreEqual(expectedResult, actualResult);
+            //accountController.Register
+        }
+        //A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot
+        [Test,
+           TestCase("abcdAAAA", "irf@uni-corvinus.hu"),
+            TestCase("1234AAAA", "irf@uni-corvinus.hu"),
+            TestCase("abcd1234", "irf@uni-corvinus.hu"),
+            TestCase("aaaA444", "irf@uni-corvinus.hu"),
+            TestCase("abcd123A", "irf@uni-corvinus.hu")
+            ]
+        public void TestValidatePassword(string password, string email)
+        {
+            //var accountController = new AccountController();
+
+            //var actualResult = accountController.ValidatePassword(password);
+
+            //Assert.AreEqual(password, actualResult);
         }
     }
 }
